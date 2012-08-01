8008d9b2e1dda6c876ed552f61044ae2a72bd014
xiongsongsong <xiongsongsong@gmail.com>
2012-08-01 12:06:34 +0800

    Modify the backup file name

--- helper/dump.js
+++ helper/dump.js
@@ -11,12 +11,17 @@ var exec = require('child_process').exec;
 exports.dump = function (req, res) {
     res.header('Content-Type', 'text/plain; charset=utf-8');
     res.write('开始备份数据库');
-    exec('mongodump -d fed -o ~/Ubuntu\\ One/Database/' + Date.now(), function (err, stdout) {
+    var date = new Date();
+    exec('mongodump -d fed -o ~/Ubuntu\\ One/Database/' +
+        date.getFullYear() + '_' +
+        (date.getMonth() + 1) + '_' +
+        date.getDate() + '_' +
+        date.getHours() + '_' +
+        date.getMinutes(), function (err, stdout) {
         if (err) {
-            res.write(err);
+            res.end(err);
         } else {
-            res.write('Success!\t' + Date.now() + '\n' + stdout);
+            res.end('Success!\t' + Date.now() + '\n' + stdout);
         }
-        res.end();
     });
 };
