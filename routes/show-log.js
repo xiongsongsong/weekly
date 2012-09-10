f8c4c91dd7e710760ff5efd99fb1b8cbce061bc2
songsong <xiongsongsong@gmail.com>
2012-09-10 16:05:33 +0800

    xss

--- routes/show-log.js
+++ routes/show-log.js
@@ -6,7 +6,10 @@
  * To change this template use File | Settings | File Templates.
  */
 
+"use strict";
+
 var DB = require('../helper/db');
+var sanitize = require('validator').sanitize;
 
 exports.show_log = function (req, res) {
     res.header('Content-Type', 'application/json; charset=utf-8');
@@ -22,11 +25,15 @@ exports.show_log = function (req, res) {
     var result = Object.create(null);
     var collection = new DB.mongodb.Collection(DB.client, 'log');
     collection.find({year:year, month:month, level:{'$gt':0}}, {}).toArray(function (err, docs) {
+        docs.forEach(function (item) {
+            Object.keys(item).forEach(function (k) {
+                if (typeof item[k] === 'string') item[k] = sanitize(item[k]).xss();
+            })
+        });
         result.documents = docs;
         result.user = require('../helper/user').frontList;
         result.serverDate = Date.now();
         if (require('./login').isLogin(req)) result.userid = req.session.userid;
         res.end(JSON.stringify(result, undefined, '\t'));
     });
-
 };
