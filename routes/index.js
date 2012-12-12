772fe1d344b8ebaeaba14cf5ec8be44331e03edc
xiongsongsong <xiongsongsong>
2012-12-12 09:34:13 +0800

    add log of history

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,12 @@
 
 exports.init = function (app) {
     require('../helper/db').open({
-        success:function () {
+        success: function () {
             require('../helper/user').init();
             console.log('数据库连接成功，开始启动路由');
             init(app);
         },
-        error:function () {
+        error: function () {
             console.log('请检查数据库是否启动');
         }
     });
@@ -21,31 +21,36 @@ function init(app) {
     //首页
     app.get('/', require('./home').index);
 
-//保存日志
+    //保存日志
     app.post('/save-log', require('./save-log').save_log);
 
-//拉取日志信息
+    //拉取日志信息
     app.get('/show_log/:date', require('./save-log').show_log);
 
-//登陆
+    //登陆
     app.post(/login(\/)?.*/, require('./login').login);
 
-//登出
+    //登出
     app.get('/log-out', require('./login').log_out);
 
-//下载报表
+    //下载报表
     app.get('/csv/:year/:month/:format?', require('./csv').download);
 
-//茶歇会的DEMO接口服务
+    //茶歇会的DEMO接口服务
     app.get('/demo/:which?', require('../helper/demo').init);
 
-//保存日志
+    //保存日志
     app.post('/save-log', require('./save-log').save_log);
 
-//拉取日志信息
+    //拉取日志信息
     app.get('/show_log/:date', require('./show-log').show_log);
 
-//本地测试使用 获取用户列表
+    //拉取日志信息
+    app.get('/show_log/:date', require('./show-log').show_log);
+
+    app.get('/history', require('./history').history);
+
+    //本地测试使用 获取用户列表
     app.get('/node/user-list', require('../helper/temp').tempUser);
 
     //本地测试使用 获取临时文件
