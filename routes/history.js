c19640138ac63e439166503f5a8f84a7895ea015
xiongsongsong <xiongsongsong>
2012-12-12 12:23:06 +0800

    beauty web page

--- routes/history.js
+++ routes/history.js
@@ -6,8 +6,6 @@
  * To change this template use File | Settings | File Templates.
  */
 
-
-
 "use strict";
 
 var DB = require('../helper/db');
@@ -18,6 +16,9 @@ exports.history = function (req, res) {
     collection.find({level: {'$gt': 0}}, {}).sort([
             ['_id', 1]
         ]).toArray(function (err, docs) {
+            var first = docs[0].year + '年' + docs[0].month + '月' + docs[0].date + '日';
+            var length = docs.length - 1;
+            var end = docs[length].year + '年' + docs[length].month + '月' + docs[length].date + '日';
             var user = require('../helper/user').frontList;
             var result = {
                 docs: docs,
@@ -27,7 +28,7 @@ exports.history = function (req, res) {
             var r = Object.create(null);
 
             for (var u in user) {
-                r[user[u]['real-name']] = {};
+                r[user[u]['real-name']] = {"wangwang": user[u]['name'] };
             }
 
             result.docs.forEach(function (item) {
@@ -46,24 +47,34 @@ exports.history = function (req, res) {
 
             //每个人
             Object.keys(r).forEach(function (item, index) {
-                html += '<h1>' + item + '</h1>';
+                html += '<h1>' + item + '<span>' + r[item].wangwang + '</span>' + '</h1>';
                 //每个人每月
                 Object.keys(r[item]).forEach(function (log) {
-                    html += '<h2>' + log + '</h2>';
-                    r[item][log].forEach(function (p) {
-                        html += '<h3>' + p['page-name'].trim() + '</h3>'
-                        html += '<ul>' +
-                            (p.customer.trim() ? '<li>需求方：' + p.customer + '</li>' : '') +
-                            (p.design.trim() ? '<li>设计师：' + p.design + '</li>' : '') +
-                            (p['online-url'].trim() ? '<li>线上地址：<a href="' + p['online-url'].trim() + '" target="_blank">' + p['online-url'] + '</a></li>' : '') +
-                            (p['tms-url'].trim() ? '<li>TMS地址：<a href="' + p['tms-url'].trim() + '" target="_blank">' + p['tms-url'] + '</a></li>' : '') +
-                            (p['note'].trim() ? '<li>备注：' + p['note'] + '</li>' : '') +
-                            '</ul>';
-                    });
+                    if (log !== 'wangwang') {
+                        html += '<h2>' + log + '</h2>';
+                        r[item][log].forEach(function (p) {
+                            var onlineurl = p['online-url'].trim();
+                            var tmsurl = p['tms-url'].trim();
+                            html += '<h3>' + p['page-name'].trim() + '</h3>'
+                            html += '<ul>' +
+                                (p.customer.trim() ? '<li>需求方：' + p.customer + '</li>' : '') +
+                                (p.design.trim() ? '<li>设计师：' + p.design + '</li>' : '') +
+                                (onlineurl ? '<li>线上地址：<a href="' + (onlineurl.indexOf('http://') >= 0 ? onlineurl : 'http://' + onlineurl) + '" target="_blank">' + p['online-url'] + '</a></li>' : '') +
+                                (tmsurl ? '<li>TMS地址：<a href="' + (tmsurl.indexOf('http://') >= 0 ? tmsurl : 'http://' + tmsurl) + '" target="_blank">' + p['tms-url'] + '</a></li>' : '') +
+                                (p['note'].trim() ? '<li>备注：' + p['note'] + '</li>' : '') +
+                                '</ul>';
+                        });
+                    }
                 })
             });
 
-            res.end(html)
+            res.render('history', {
+                title: '前端业务日志',
+                layout: false,
+                body: html,
+                first: first,
+                end: end
+            });
 
         });
 };
