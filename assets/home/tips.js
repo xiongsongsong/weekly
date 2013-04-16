a5eb03f38a0ce6a4d6988268b0c96e6461702419
mydou <252202445@qq.com>
2013-04-16 15:31:04 +0800

    增加WTP的文章链接

--- assets/home/tips.js
+++ assets/home/tips.js
@@ -7,6 +7,26 @@
  */
 
 define(function (require, exports, module) {
-    var span = KISSY.DOM.get('span', '#header');
-    if (span)  KISSY.Anim(KISSY.DOM.get('span', '#header'), {top: '0px'}, 1.2, 'elasticBoth').run();
+    var S = KISSY, span = S.DOM.get('span', '#header');
+    S.Anim(S.DOM.get('span', '#header'), {top: '0px'}, 1.2, 'elasticBoth').run();
+
+
+    S.jsonp('https://api.github.com/repos/lifesinger/lifesinger.github.com/issues?state=opend', function (data) {
+
+        var link = [];
+        for (var j = 0; j < data.data.length; j++) {
+            link.push('<li><a target="_blank" href="' + data.data[j].html_url + '">' + data.data[j].title + '</a></li>')
+            if (j > 5) break;
+        }
+
+        link.push('<li style="border-top:solid 1px #ccc;"><a target="_blank" href="http://www.atatech.org/article/detail/7259/392">JavaScript中的对象</a></li>')
+        link.push('<li><a target="_blank" href="http://msdn.microsoft.com/zh-cn/magazine/cc163419.aspx">JavaScript 基础</a></li>')
+
+
+        var ul = S.query('#sidebar-group ul');
+        ul = S.one(ul[ul.length - 1]);
+        ul.html(link.join(''));
+
+    });
+
 });
