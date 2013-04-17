d4fa38d1da791895dfd1a4b5acdc4e44f6568873
mydou <252202445@qq.com>
2013-04-17 09:10:44 +0800

    让服务器去获取wtp的文章内容。

--- routes/home.js
+++ routes/home.js
@@ -7,13 +7,44 @@
  */
 
 'use strict';
+var https = require('https');
+var BufferHelper = require('bufferhelper');
+
+function getWtp() {
+    https.get('https://api.github.com/repos/lifesinger/lifesinger.github.com/issues?state=opend',function (res) {
+
+        var bufferHelper = new BufferHelper();
+        res.on('data', function (d) {
+            bufferHelper.concat(d);
+        });
+
+        res.on('end', function () {
+            var html = bufferHelper.toBuffer().toString();
+            try {
+                exports.wtp = JSON.parse(html);
+            } catch (e) {
+                console.log('wtp服务器错误');
+            }
+            setTimeout(getWtp, 1800000)
+        });
+
+    }).on('error', function (e) {
+            console.error(e);
+            setTimeout(getWtp, 1800000)
+        });
+}
+
+getWtp();
 
 exports.index = function (req, res) {
     res.render('index', {
-        title: '前端业务日志',
-        isLogin: require('./login').isLogin(req),
-        date: new Date(),
-        username: req.session.username,
-        sessionID: req.sessionID
-    });
+            title: '前端业务日志',
+            isLogin: require('./login').isLogin(req),
+            date: new Date(),
+            username: req.session.username,
+            sessionID: req.sessionID,
+            wtp: exports.wtp
+        }
+    )
+
 };
