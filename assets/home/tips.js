4aaf33f66db857f45226c0348bfdbacbfe353354
mydou <252202445@qq.com>
2013-04-25 08:29:21 +0800

    使用js来维护推荐列表

--- assets/home/tips.js
+++ assets/home/tips.js
@@ -10,15 +10,18 @@ define(function (require, exports, module) {
     var S = KISSY, span = S.DOM.get('span', '#header');
     S.Anim(S.DOM.get('span', '#header'), {top: '0px'}, 1.2, 'elasticBoth').run();
 
+    var link = [];
+    link.push('<li><a target="_blank" href="https://github.com/lifesinger/lifesinger.github.com/issues/105">最难的HTML+CSS</a></li>');
+    link.push('<li><a target="_blank" href="http://msdn.microsoft.com/zh-cn/magazine/cc163419.aspx">JavaScript 基础</a></li>');
+    link.push('<li><a target="_blank" href="https://github.com/TooooBug/javascript.patterns/blob/master/chapter4.markdown#-1">函数</a></li>');
+    link.push('<li><a target="_blank" href="http://www.atatech.org/article/detail/7259/392">对象</a></li>')
+    link.push('<li><a target="_blank" href="http://www.cnblogs.com/pifoo/archive/2011/05/23/webkit-touch-event-1.html">指尖上的JS</a></li>');
+    link.push('<li><a target="_blank" href="http://justjavac.com/">justjavac(迷渡) </a></li>');
+    link.push('<li><a target="_blank" href="https://github.com/lifesinger/lifesinger.github.com/issues?state=open">玉伯的博客 </a></li>');
 
-
-        var link = [];
-        link.push('<li style="border-top:dotted 1px #ccc;"><a target="_blank" href="http://www.atatech.org/article/detail/7259/392">JavaScript中的对象</a></li>')
-        link.push('<li><a target="_blank" href="http://msdn.microsoft.com/zh-cn/magazine/cc163419.aspx">JavaScript 基础</a></li>')
-
-        var ul = S.query('#sidebar-group ul');
-        ul = S.one(ul[ul.length - 1]);
-        ul.append(link.join(''));
+    var ul = S.query('#sidebar-group ul');
+    ul = S.one(ul[ul.length - 1]);
+    ul.append(link.join(''));
 
 
 });
--- routes/home.js
+++ routes/home.js
@@ -7,34 +7,6 @@
  */
 
 'use strict';
-var https = require('https');
-var BufferHelper = require('bufferhelper');
-
-function getWtp() {
-    https.get('https://api.github.com/repos/lifesinger/lifesinger.github.com/issues?state=opend',function (res) {
-
-        var bufferHelper = new BufferHelper();
-        res.on('data', function (d) {
-            bufferHelper.concat(d);
-        });
-
-        res.on('end', function () {
-            var html = bufferHelper.toBuffer().toString();
-            try {
-                exports.wtp = JSON.parse(html);
-            } catch (e) {
-                console.log('wtp服务器错误');
-            }
-            setTimeout(getWtp, 1800000)
-        });
-
-    }).on('error', function (e) {
-            console.error(e);
-            setTimeout(getWtp, 1800000)
-        });
-}
-
-getWtp();
 
 exports.index = function (req, res) {
     res.render('index', {
@@ -42,8 +14,7 @@ exports.index = function (req, res) {
             isLogin: require('./login').isLogin(req),
             date: new Date(),
             username: req.session.username,
-            sessionID: req.sessionID,
-            wtp: exports.wtp
+            sessionID: req.sessionID
         }
     )
 
